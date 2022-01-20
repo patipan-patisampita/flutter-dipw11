0eaa54351a141bc41c566befefcda29d50aeedf9
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-20 11:25:11 +0700

    update

--- lib/pages/grid_view_demo.dart
+++ lib/pages/grid_view_demo.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'my_card.dart';
+
 class GridViewDemo extends StatelessWidget {
   const GridViewDemo({
     Key? key,
@@ -12,7 +14,46 @@ class GridViewDemo extends StatelessWidget {
         title: Text("Flutter Grid View"),
         centerTitle: true,
       ),
-      body: Center(child: const Text("MyApp")),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20, 50, 20, 50),
+          child: GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            children: [
+              Container(
+                decoration: BoxDecoration(color: Colors.red),
+                padding: EdgeInsets.all(6.0),
+                child: MyCard(
+                  title:'Flutter',
+                  subTitle: 'Version 2.5',
+                  icon:Icons.home ,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(color: Colors.red),
+                padding: EdgeInsets.all(6.0),
+                child: MyCard(
+                  title:'React Typescript',
+                  subTitle: 'Version 11.5',
+                  icon:Icons.home ,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(6.0),
+                child: Center(child: Text('3')),
+                color: Colors.red[100],
+              ),
+              Container(
+                padding: EdgeInsets.all(6.0),
+                child: Center(child: Text('4')),
+                color: Colors.red[100],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
