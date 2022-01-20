b994379a2b4ddcf8df1b56d83d673c42b2ff5c7c
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-20 14:16:32 +0700

    first init

--- lib/pages/grid_view_dynamic.dart
+++ lib/pages/grid_view_dynamic.dart
@@ -18,6 +18,7 @@ List<CardModel> cards = [
   CardModel(title: 'title-3', subTitle: 'sub-3', icon: Icons.alarm),
   CardModel(title: 'title-4', subTitle: 'sub-4', icon: Icons.account_balance),
   CardModel(title: 'title-5', subTitle: 'sub-5', icon: Icons.mobile_friendly),
+  CardModel(title: 'title-5', subTitle: 'sub-5', icon: Icons.air),
 ];
 
 class GridViewDynamic extends StatelessWidget {
@@ -30,23 +31,48 @@ class GridViewDynamic extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(20),
           child: GridView.count(
-            crossAxisCount: 2,
+            crossAxisCount: 3,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20,
-            children: List.generate(
-              cards.length,
-              (index) {
+            children: List.generate(cards.length, (index) {
                 var model = cards[index];
-                return Container(
-                  decoration: BoxDecoration(color: Colors.purple),
-                  child: MyCard(
-                    title: model.title,
-                    subTitle: model.subTitle,
-                    icon: model.icon,
-                  ),
-                );
+                return CardAction(model: model);
               },
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardAction extends StatelessWidget {
+  const CardAction({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  final CardModel model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.deepOrange,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      padding: EdgeInsets.all(8),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+          onTap: () {
+            print(model.title);
+          },
+          child: MyCard(
+            title: model.title,
+            subTitle: model.subTitle,
+            icon: model.icon,
           ),
         ),
       ),
